d0ee3d0e487909d48c9bd7c8bec4c0ce052450dc
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-01 23:03:05 +0800

    123

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
     def index
-        @videos = [
+    @videos = [
         {
             "活動": "蝦幣寶箱&每日簽到",
             "連結": "https://shp.ee/ii4iivjsxey",
