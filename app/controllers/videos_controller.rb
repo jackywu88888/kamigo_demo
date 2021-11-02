210ebc3459d22084ace267f7f9bb8f49dea4a324
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-02 15:17:55 +0800

    111003

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,7 +5,7 @@ class VideosController < ApplicationController
             "活動": "蝦幣寶箱/每日簽到",
             "連結": "https://shp.ee/ii4iivjsxey",
             "完美": "https://risu.io/yUrX",
-        }
+        },
         ]
           end
         end
