46413ceeee4cf7956132007c1dd9bb2bb1833e5e
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-02 15:32:21 +0800

    202110027

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,9 +2,33 @@ class VideosController < ApplicationController
     def index
         @videos = [
         {
-            "活動": "蝦幣寶箱/每日簽到",
-            "連結": "https://shp.ee/ii4iivjsxey",
-            "完美": "https://risu.io/yUrX",
+          "活動": "蝦幣寶箱/每日簽到",
+          "連結": "https://shp.ee/ii4iivjsxey",
+          "完美": "https://risu.io/yUrX",
+        },
+        {
+          "活動": "[飛比簽到]",
+          "連結": "https://rotf.lol/Feebee",,
+        },
+        {
+          "活動": "[H5點就玩]",
+          "連結": "簽到在首頁右上角",
+          "完美": "https://pse.is/S9UZ2",
+        },
+        {
+          "活動": "[tada台灣車主協會]",
+          "連結": "每天打卡",
+          "完美": "line://oaMessage/@tada_car/%23我要打卡",
+        },
+        {
+          "活動": "[商略問卷代發]",
+          "連結": "line://oaMessage/@032okdnb/?%40填寫問卷",
+          "完美": "line://oaMessage/@032okdnb/?%40試試手氣",
+        },
+        {
+          "活動": "BF.上方.遊戲.簽到簿",
+          "連結": "每週一中午12時開抽500元零用金",
+          "完美": "https://lihi1.com/nUlLa",
         },
         ]
           end
