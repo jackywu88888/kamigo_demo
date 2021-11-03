262724aeb6aed31a50274e2ebe4b3fe2cd18e168
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-03 12:54:58 +0800

    20211103006

--- app/controllers/weather2_controller.rb
+++ app/controllers/weather2_controller.rb
@@ -1,7 +1,5 @@
 class WeatherController < ApplicationController
     def show2
-      @temperature = "type"  
-      "type": "text",
-      "text": "LINE Bot 串接成功！"
+      @temperature = 29
     end
   end
