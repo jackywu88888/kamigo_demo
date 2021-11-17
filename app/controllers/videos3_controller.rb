e957513c5dbbd1c7ff932d3406d756b52675758a
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-17 16:05:08 +0800

    242

--- app/controllers/videos3_controller.rb
+++ app/controllers/videos3_controller.rb
@@ -1,14 +1,30 @@
 class Videos3Controller < ApplicationController
-    def index
-      @videos3 = [
-    {
-      "活動" : "全家一暖好茶轉轉樂全家一暖好茶轉轉樂",
-      "連結" : "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
-    },
-    {
-        "活動" : "全家一暖好茶轉轉樂",
-        "連結" : "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
-    },
-    ]
-end
-end
+  def index
+      @videos2 = [
+      {
+        "活動": "11/12-11/21 簽到分享最高送$40momo幣,momo幣將於簽到後立刻歸戶,使用期限至 11/21 23:59,momo-年終感恩季-天天簽到 - momo購物網" ,
+        "連結": "https://momo.dm/Fu3EIU" ,
+      },
+      {
+        "活動": "11/1-11/14每人每週(週一至週日)限參加一次",
+        "連結": "看看買-1101-1114 1111豪買節(A)",
+        "完美": "https://momo.dm/FmBuyu",
+      },
+      {
+        "活動": "11/1-11/14",
+        "連結": "1111豪買節-積分賽(A)",
+        "完美": "https://momo.dm/ayyNjf",
+      },
+      {
+        "活動": "活動總覽",
+        "連結": "https://momo.dm/N6uau6",
+        "完美": "",
+      },
+      {
+        "活動": "折價券天天免費領",
+        "連結": "https://momo.dm/qRb2j3",
+        "完美": "",
+      },
+      ]
+        end
+      end
