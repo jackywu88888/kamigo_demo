86847151f5adf01ff4e4b624622a091aa8a9fccb
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-17 14:43:24 +0800

    228

---
+++ app/controllers/material1.controller.rb
@@ -0,0 +1,16 @@
+class Videos2Controller < ApplicationController
+    def index
+@material1s = [
+    {
+      "網址" => "全家一暖好茶轉轉樂",
+      "影片" => "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
+      "成果" => 300,
+    },
+      {
+      "網址" => 100,
+      "影片" => 200,
+      "成果" => 300,
+    },
+    ]
+end
+end
