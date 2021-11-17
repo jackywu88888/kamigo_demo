9c66042f08b250cd9a36f71ed44c75161b389e6b
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-17 15:42:23 +0800

    237

--- app/controllers/material1.controller.rb
+++ app/controllers/material1.controller.rb
@@ -5,7 +5,7 @@ class Material1Controller < ApplicationController
       "活動" : "全家一暖好茶轉轉樂全家一暖好茶轉轉樂",
       "連結" : "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
     },
-      {
+    {
         "活動" : "全家一暖好茶轉轉樂",
         "連結" : "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
     },
