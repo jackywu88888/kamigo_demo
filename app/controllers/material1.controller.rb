b55c8669de225f7db0602ce5c96c7c086fa04519
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-17 15:37:31 +0800

    236

--- app/controllers/material1.controller.rb
+++ app/controllers/material1.controller.rb
@@ -2,7 +2,7 @@ class Material1Controller < ApplicationController
     def index
 @material1s = [
     {
-      "活動" : "全家一暖好茶轉轉樂\n\n全家一暖好茶轉轉樂",
+      "活動" : "全家一暖好茶轉轉樂全家一暖好茶轉轉樂",
       "連結" : "https://line.me/R/app/1655769181-xePA5be5?gid=bV4JNa4n",
     },
       {
