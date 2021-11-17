83adfeaa118945857cac8edf4fce7f9851ac04ab
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-11-17 11:21:17 +0800

    202111170199

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 # get "momo", to: "videos2#index"
   get "MOMO簽到", to: "home#material"
   get "momo", to: "home#material"
-  get "lov", to: "home#point"
+  get "lov", to: "home#material"
   
   # menu
 # get "index", to: "home#index"
