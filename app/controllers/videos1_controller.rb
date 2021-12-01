643bf70719fb61c8f581c56b9957f800b6ba22aa
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-12-01 11:22:44 +0800

    404

--- app/controllers/videos1_controller.rb
+++ app/controllers/videos1_controller.rb
@@ -11,15 +11,23 @@ class Videos1Controller < ApplicationController
         },
         {
           "活動": "[H5點就玩]簽到在首頁右上角",
-          "連結": "https://pse.is/S9UZ2",
+          "連結": "積分可以兌換商品、咖啡、LINE POINTS。\\n\\nhttps://pse.is/S9UZ2",
         },
         {
-          "活動": "[tada台灣車主協會]每天打卡",
+          "活動": "【Y5遊戲】 （H5/Yahoo版）簽到、任務中心回答問題累積積分。",
+          "連結": "積分可以兌換商品、咖啡、LINE POINTS。\\n\\nhttps://yahoo.wakool.net/welfare-center/quest",
+        },
+        {
+          "活動": "[tada台灣車主協會]每天打卡\\n獲得5績分,500積分➡️5LP",
           "連結": "http://line.me/R/oaMessage/@tada_car/%23我要打卡",
         },
         {
           "活動": "[商略問卷代發]",
           "連結": "http://line.me/R/oaMessage/@032okdnb/?%40填寫問卷\\n\\nhttp://line.me/R/oaMessage/@032okdnb/?%40試試手氣",
+        },
+        {
+          "活動": "yes123簽到（每月簽到7天）",
+          "連結": "https://m.yes123.com.tw/admin/freego_2019/activity.asp",
         },
         {
           "活動": "BF.上方.遊戲.簽到簿\\n\\n每週一中午12時開抽500元零用金",
@@ -34,7 +42,7 @@ class Videos1Controller < ApplicationController
           "連結": "https://us9e5.app.goo.gl/SKrcB",
         },
         {
-          "活動": "那快來參加遠傳打卡活動吧～",
+          "活動": "那快來參加遠傳打卡活動吧～(更新版本之後需要定位，無法使用)",
           "連結": "https://bit.ly/每天打卡領遠傳幣—LINEPoints賺點數社群分享",
         },
         {
@@ -42,23 +50,15 @@ class Videos1Controller < ApplicationController
           "連結": "https://www.feds.com.tw/tw/CardEvent/MemberCardDetail/1057\\n\\nhttps://www.feds.com.tw/app/fedsappdownload.asp",
         },
         {
-          "活動": "金石堂會員活動2021/01/01~2021/12/31",
-          "連結": "https://www.kingstone.com.tw/ksmember/home/#dailycheck",
-        },
-        {
-          "活動": "中信亮點APP 每日簽到！2021/08/01~2021/12/31 ",
-          "連結": "https://insightctbcsec.page.link/tqgE",
-        },
-        {
-          "活動": "特力屋簽到11/1-11，最高 110 點！",
-          "連結": "https://www.trplus.com.tw/event/trplus/211101_signt",
+          "活動": "UUPON APP每日簽到 賺點",
+          "連結": "https://appevts.uupon.com/dailysignin/page",
         },
     ]
 
     @keyword = params[:keyword]
 
     if @keyword.present?
-        @videos.filter! do |video|
+        @videos1.filter! do |video|
            video[:活動].include? @keyword
         end
       end
