44994d91caa8b198afaf9ae668adce5d3ce7da34
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2021-12-11 14:26:37 +0800

    436

--- app/controllers/videos5_controller.rb
+++ app/controllers/videos5_controller.rb
@@ -3,8 +3,8 @@ class Videos5Controller < ApplicationController
         @videos5 = [
           {
             "題目": "長期投資策略就是抱緊處理嗎?" ,
-            "答案": "基本題:B.A.A.A.A\\n\\n進階題:A.A.B.B.A\\n\\n應用題:B.A.A.A.A\\n\\n情境題:A.A.A.A.B
-            ;" ,
+            "答案": "基本題:B.A.A.A.A\\n\\n進階題:A.A.B.B.A\\n\\n應用題:B.A.A.A.A\\n\\n情境題:A.A.A.A.B;",
+          },
         ]
 
         @keyword = params[:keyword]
