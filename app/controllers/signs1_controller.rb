7fb7568cb928b24e542b225325ac3fec1c464785
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-07 22:43:02 +0800

    521

--- app/controllers/signs1_controller.rb
+++ app/controllers/signs1_controller.rb
@@ -6,10 +6,10 @@ class Signs1Controller < ApplicationController
           "名稱": "《蝦幣寶箱》",
           "時間": "永久活動",
           "連結": "https://shp.ee/ii4iivjsxey",
-          "圖片": "https://cf.shopee.tw/file/https://cf.shopee.tw/file/98ccb05e0677043a576362ea7a572fb4",
+          "圖片": "https://s3-ap-northeast-1.amazonaws.com/upload.potatomedia.co/articles/potato_59b2ea24-55be-4a09-a4f9-afb6edc48e6c_c451bcbb1b00e0f833459d7d215fd6289f37e038.png",
         },
         {
-          "活動": "可以從蝦皮購物APP ：「我的」頁面 >「我的蝦幣」> 蝦幣獎勵頁面 >「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
+          "活動": "可以從蝦皮購物APP ：「我的」頁面 ▶️「我的蝦幣」▶️ 蝦幣獎勵頁面 ▶️「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
           "名稱": "《蝦幣每日簽到》",
           "時間": "永久活動",
           "連結": "https://risu.io/yUrX",
