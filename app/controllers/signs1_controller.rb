d00422d6d1295f6353c786e36c357790c087d5f1
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-08 16:45:19 +0800

    530

--- app/controllers/signs1_controller.rb
+++ app/controllers/signs1_controller.rb
@@ -23,7 +23,7 @@ class Signs1Controller < ApplicationController
           "圖片": "https://ssl.feebee.com.tw/point/task_01_2x.png"
         },
         {
-          "活動": "簽到在首頁右上角，積分可以兌換商品、咖啡、LINE POINTS等商品",
+          "活動": "簽到在i好康右上角，積分可以兌換商品、咖啡、LINE POINTS等商品",
           "名稱": "H5點就玩",
           "時間": "永久活動",
           "連結": "https://iplay.wakool.net/?utm_source=LINE&utm_medium=GAME&utm_campaign=LINEGAME1015&openExternalBrowser=1",
@@ -35,6 +35,27 @@ class Signs1Controller < ApplicationController
           "時間": "永久活動",
           "連結": "https://yahoo.wakool.net/welfare-center/quest",
           "圖片": "https://yahoo.wakool.net/cdn/platform/yahoo/img/default/y5_share_og.jpg"
+        },
+        {
+          "活動": "(H5/全家便利商店版)簽到、任務中心回答問題累積積分。🎁積分可以兌換霜淇淋、咖啡、衛生紙。",
+          "名稱": "全家好好玩",
+          "時間": "永久活動",
+          "連結": "https://bit.ly/3pXTgES",
+          "圖片": "https://images.wakool.net/fami/logo-1435x543.png"
+        },
+        {
+          "活動": "每天打卡\\n獲得5績分,500積分➡️5LP",
+          "名稱": "tada台灣車主協會",
+          "時間": "永久活動",
+          "連結": "http://line.me/R/oaMessage/@tada_car/%23我要打卡",
+          "圖片": "https://is4-ssl.mzstatic.com/image/thumb/Purple114/v4/a3/cd/36/a3cd3667-3277-7123-ad6a-2ca29b302489/source/512x512bb.jpg"
+        },
+        {
+        "活動": "商略問卷獎酬平台是專門提供問卷填寫與贈獎\\nhttp://line.me/R/oaMessage/@032okdnb/?%40試試手氣",
+        "名稱": "商略問卷代發",
+        "時間": "永久活動",
+        "連結": "http://line.me/R/oaMessage/@032okdnb/?%40填寫問卷",
+        "圖片": "https://static.wixstatic.com/media/c5667f_3828482036744e0884407e6b560f0cad~mv2.png/v1/fill/w_2500,h_2500,al_c/c5667f_3828482036744e0884407e6b560f0cad~mv2.png"
         },
     ]
 
