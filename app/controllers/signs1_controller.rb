ef2ef01c158e836741835ae47481d4eb3d4e714e
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-08 16:17:04 +0800

    527

--- app/controllers/signs1_controller.rb
+++ app/controllers/signs1_controller.rb
@@ -9,11 +9,32 @@ class Signs1Controller < ApplicationController
           "圖片": "https://play-lh.googleusercontent.com/krmhwGF9KYSwvJ5hBTh2MO_QSDI3S7Mad0gpRvRKNGYzHpHQB-WMwIQYxWYkW3YNHvI"
         },
         {
-          "活動": "從蝦皮購物APP ：「我的」頁面 ▶️「我的蝦幣」▶️ 蝦幣獎勵頁面 ▶️「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
+          "活動": "從蝦皮購物APP：「我的」頁面▶️「我的蝦幣」▶️蝦幣獎勵頁面▶️「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
           "名稱": "蝦幣簽到",
           "時間": "永久活動",
           "連結": "https://risu.io/yUrX",
           "圖片": "https://s.zimedia.com.tw/s/LqA69o-1"
+        },
+        {
+          "活動": "飛比點數是由飛比價格發行的點數，透過在指定平台內完成各種任務，即可獲得點數。獲得的點數可以在飛比價格App內兌換各種禮物。",
+          "名稱": "飛比簽到",
+          "時間": "永久活動",
+          "連結": "https://rotf.lol/Feebee",
+          "圖片": "https://ssl.feebee.com.tw/point/task_01_2x.png"
+        },
+        {
+          "活動": "簽到在首頁右上角，積分可以兌換商品、咖啡、LINE POINTS等商品",
+          "名稱": "H5點就玩",
+          "時間": "永久活動",
+          "連結": "https://iplay.wakool.net/?utm_source=LINE&utm_medium=GAME&utm_campaign=LINEGAME1015&openExternalBrowser=1",
+          "圖片": "https://iplay.wakool.net/img/logo.png"
+        },
+        {
+          "活動": "(H5/Yahoo版)簽到、任務中心回答問題累積積分。積分可以兌換商品、咖啡、LINE POINTS。",
+          "名稱": "Y5遊戲",
+          "時間": "永久活動",
+          "連結": "https://yahoo.wakool.net/welfare-center/quest",
+          "圖片": "https://yahoo.wakool.net/cdn/platform/yahoo/img/y5_games_logo_152x62.svg"
         },
     ]
 
@@ -21,7 +42,7 @@ class Signs1Controller < ApplicationController
 
     if @keyword.present?
         @signs1.filter! do |sign|
-            sign[:活動].include? @keyword
+            sign[:名稱].include? @keyword
       end
     end
     @signs1 = @signs1.first(12)
