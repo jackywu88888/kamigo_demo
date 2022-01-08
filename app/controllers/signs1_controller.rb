ee4459460aa7fd01d547d2c5018d8a6765618ac2
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-08 15:14:51 +0800

    525

--- app/controllers/signs1_controller.rb
+++ app/controllers/signs1_controller.rb
@@ -3,14 +3,14 @@ class Signs1Controller < ApplicationController
       @signs1 = [
         {
           "活動": "蝦皮寶箱，人人有獎，點就領蝦幣",
-          "名稱": "《蝦幣寶箱》",
+          "名稱": "蝦幣寶箱",
           "時間": "永久活動",
           "連結": "https://shp.ee/ii4iivjsxey",
           "圖片": "https://play-lh.googleusercontent.com/krmhwGF9KYSwvJ5hBTh2MO_QSDI3S7Mad0gpRvRKNGYzHpHQB-WMwIQYxWYkW3YNHvI"
         },
         {
-          "活動": "可以從蝦皮購物APP ：「我的」頁面 ▶️「我的蝦幣」▶️ 蝦幣獎勵頁面 ▶️「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
-          "名稱": "《蝦幣每日簽到》",
+          "活動": "從蝦皮購物APP ：「我的」頁面 ▶️「我的蝦幣」▶️ 蝦幣獎勵頁面 ▶️「今日簽到獲得蝦幣」即可賺取當日蝦幣獎勵",
+          "名稱": "蝦幣簽到",
           "時間": "永久活動",
           "連結": "https://risu.io/yUrX",
           "圖片": "https://s.zimedia.com.tw/s/LqA69o-1"
