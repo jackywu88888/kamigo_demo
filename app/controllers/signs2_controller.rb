123236b11dcbc79b67283dbfa8d99692bb67d101
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-09 12:46:16 +0800

    575

--- app/controllers/signs2_controller.rb
+++ app/controllers/signs2_controller.rb
@@ -9,11 +9,11 @@ class Signs2Controller < ApplicationController
         "圖片": "https://tw.adnew.beanfun.com/ad_image/BFWebCommon_MiddleImage/20200921121720.png"
       },
       {
-        "活動": "那快來參加遠傳打卡活動吧～(更新版本之後需要定位，無法使用)",
+        "活動": "那快來參加遠傳打卡活動吧(更新版本之後需要定位，無法使用)",
         "名稱": "遠傳打卡活動",
         "時間": "永久活動",
         "連結": "https://reurl.cc/ZrQGEM",
-        "圖片": "https://content.shopback.com/tw/wp-content/uploads/2020/05/25171130/buy123-1200x671.png",
+        "圖片": "https://content.shopback.com/tw/wp-content/uploads/2020/05/25171130/buy123-1200x671.png"
       },
       {
         "活動": "安麗每日簽到拿點數",
