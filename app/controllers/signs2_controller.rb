5713d27aa73b564d022323ea83fd5fb606c3d944
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-09 12:22:14 +0800

    567

--- app/controllers/signs2_controller.rb
+++ app/controllers/signs2_controller.rb
@@ -1,11 +1,11 @@
 class Signs2Controller < ApplicationController
   def index
     @signs2 = [
-      {
+        {
           "活動": "每BF.上方.遊戲.簽到簿\\n每週一中午12時開抽500元零用金",
           "名稱": "beanfun",
           "時間": "永久活動",
-          "連結": "https://lihi1.com/nUlLa",
+          "連結": "https://reurl.cc/ZG2qoW",
           "圖片": "https://tw.adnew.beanfun.com/ad_image/BFWebCommon_MiddleImage/20200921121720.png"
         },
         {
