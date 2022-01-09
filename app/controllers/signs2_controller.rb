2822141304676edcab5f999d4e14c0c87110f16f
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-09 12:14:18 +0800

    563

--- app/controllers/signs2_controller.rb
+++ app/controllers/signs2_controller.rb
@@ -19,7 +19,7 @@ class Signs2Controller < ApplicationController
           "活動": "安麗每日簽到拿點數",
           "名稱": "安麗每日簽到",
           "時間": "永久活動",
-          "連結": "https://liff.line.me/1655451151-RbE08al5",
+          "連結": "http://line.me/R/oaMessage/@amway",
           "圖片": "https://images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/5bf41cca049f03cdc7e842db2201172d6cc1a6b173e8db293a3b880ecc5836561616582409012.jpg"
         },
 #        {
