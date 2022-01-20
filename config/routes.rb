d51bb56dfb15a99c9200c408088ce81c17d4b549
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-20 20:28:15 +0800

    0120信用卡023

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
   get "7-11超商自動加值", to: "creditcards8#index"
   get "全家超商自動加值", to: "creditcards9#index"
   get "萊爾富/OK超商自動加值", to: "creditcards10#index"
+  get "加油", to: "creditcards11#index"
 
 #  get "網購", to: "videos10#index"
 #  get "餐廳", to: "videos11#index"
@@ -72,7 +73,7 @@ Rails.application.routes.draw do
 #  get "7-11超商/電子票證可參照自動加值各項行動支付可參照行動支付", to: "videos20#index"
 #  get "全家超商/電子票證可參照自動加值各項行動支付可參照行動支付", to: "videos21#index"
 #  get "萊爾富OK超商/電子票證可參照自動加值各項行動支付可參照行動支付", to: "videos22#index"
-  get "加油", to: "videos23#index"
+#  get "加油", to: "videos23#index"
   get "北捷/公車", to: "videos24#index"
   get "台鐵", to: "videos25#index"
   get "高鐵", to: "videos26#index"
