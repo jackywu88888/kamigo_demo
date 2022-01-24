16e68bb3c08970cdd8361183fcefba6d415ef1dc
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-24 17:18:08 +0800

    0124line_Tv065

--- app/controllers/signs2_controller.rb
+++ app/controllers/signs2_controller.rb
@@ -9,13 +9,6 @@ class Signs2Controller < ApplicationController
         "圖片": "https://tw.adnew.beanfun.com/ad_image/BFWebCommon_MiddleImage/20200921121720.png"
       },
       {
-        "活動": "那快來參加遠傳打卡活動吧(更新版本之後需要定位，無法使用)",
-        "名稱": "遠傳打卡活動",
-        "時間": "永久活動",
-        "連結": "https://reurl.cc/ZG2qoW",
-        "圖片": "https://www.fetnet.net/images/fet_big.jpg"
-      },
-      {
         "活動": "安麗每日簽到拿點數",
         "名稱": "安麗每日簽到",
         "時間": "永久活動",
