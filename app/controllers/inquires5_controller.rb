a3d892f0b774701e2b75fbcfaa6df9dfadcb6598
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-01-26 20:29:15 +0800

    0125新增LINETV184

--- app/controllers/inquires5_controller.rb
+++ app/controllers/inquires5_controller.rb
@@ -2,6 +2,15 @@ class Inquires5Controller < ApplicationController
     def index
       @inquires5 = [
         {
+          "活動": "強棒好劇過新年！完成任務得LINE POINTS 5點",
+          "活動主題": "追劇贈點",
+          "限量或VIP": "此檔LINE POINTS 活動點數\\n數量有限，活動期間送完為止",
+          "名稱": "《鬼滅之刃第二集》",
+          "時間": "2022/01/27~2022/02/02",
+          "影片": "https://bnc.lt/MuAd/C3kdaEqq6mb",
+          "圖片": "https://img.linetv.tw/large/campaign/220127_lastdrama.jpg",
+        },
+        {
           "活動": "「虎」伊看好戲多賺點✨ 完成任務連續七天觀看指定戲劇一集得 10 點，VIP 會員可得 20 點",
           "活動主題": "追劇贈點",
           "限量或VIP": "VIP 會員加碼再抽【iPhone 12 Pro (128GB 太平洋藍）】手機乙台",
@@ -28,14 +37,6 @@ class Inquires5Controller < ApplicationController
           "影片": "https://bnc.lt/MuAd/gcbegVuoMmb",
           "圖片": "https://img.linetv.tw/large/campaign/2022011301.jpg",
         },
-      #  {
-      #    "活動": "1月新劇速報，完成追劇任務得 LINE POINTS 5 點！",
-      #    "活動主題": "追劇贈點",
-      #    "名稱": "《進擊的巨人The Final Season Part 2》",
-      #    "時間": "2022/01/15~2022/01/22",
-      #    "影片": "https://bnc.lt/MuAd/P3Ay3dqzFmb",
-      #    "圖片": "https://img.linetv.tw/large/campaign/campaign_points_2201151.png",
-      #  },
 
     ]
 
