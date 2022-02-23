8d1b5b5dbabefc2800bda618dfd72fea6c92ec57
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-02-23 21:46:28 +0800

    0223發票0287

--- app/controllers/invoices1_controller.rb
+++ app/controllers/invoices1_controller.rb
@@ -3,7 +3,7 @@ class Invoices1Controller < ApplicationController
       @invoices1 = [
         {
           "發票商家": "OPEN POINT",
-          "步驟教學": "準備動作：1.下載OPEN POINT App2.登入OPEN POINT的會員1:點選手機條碼載具設定",
+          "步驟教學": "1:點選手機條碼載具設定\n\n準備動作：\n1.下載OPEN POINT App\n2.登入OPEN POINT的會員",
           "圖片顯示": "https://live.staticflickr.com/65535/51897467284_f517b31fc9_c.jpg", 
         },
         {
