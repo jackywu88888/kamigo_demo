1100b36a8e5972a125a8d670ecd76126ffe78c48
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-03-16 09:39:15 +0800

    0316星城教學0319

--- app/controllers/inquires31_controller.rb
+++ app/controllers/inquires31_controller.rb
@@ -4,7 +4,7 @@ class Inquires31Controller < ApplicationController
         {
           "遊戲商家": "星城教學",
           "步驟教學": "1:點選下面網址領取10,000振興幣\nhttps://www.xin-stars.com/2021GetCash?sharecode=SUTY9WW0",
-          "圖片顯示": "https://live.staticflickr.com/65535/51941221779_c6e3069e24_c.jpg", 
+          "圖片顯示": "https://www.xin-stars.com/2021GetCash?sharecode=SUTY9WW0", 
         },
         {
           "遊戲商家": "星城教學",
