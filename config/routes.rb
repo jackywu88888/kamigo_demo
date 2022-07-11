32b7f0fb17c65d4138dcc79c0f485c07e368919f
jackywu88888 <93433022+jackywu88888@users.noreply.github.com>
2022-07-11 17:41:44 +0800

    0711環保0418

--- config/routes.rb
+++ config/routes.rb
@@ -125,7 +125,7 @@ Rails.application.routes.draw do
 #  get "學費", to: "videos41#index"
 
 
-  get "星城", to: "inquires32#index"  
+#  get "星城", to: "inquires32#index"  
   get "line_tv", to: "inquires5#index"
   get "關鍵字", to: "home#points5"
   get "環保集點", to: "home#points6"
